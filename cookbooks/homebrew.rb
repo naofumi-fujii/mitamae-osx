64ad546fc2264e1b733b9e874a4c1cf0f921ec39
naofumi-fujii <naofumi.fujii@gmail.com>
2020-07-17 11:04:07 +0900

    now its shell script

--- cookbooks/homebrew.rb
+++ cookbooks/homebrew.rb
@@ -1,6 +1,5 @@
-execute 'Install Homebrew' do
+execute "Install Homebrew" do
   user node[:user]
-  command 'yes "" | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
-  not_if 'test $(which /usr/local/bin/brew)'
+  command 'yes "" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"'
+  not_if "test $(which /usr/local/bin/brew)"
 end
-
