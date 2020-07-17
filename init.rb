4071d193a1fbc845d555d46fd968a8ff06e98dbc
naofumi-fujii <naofumi.fujii@gmail.com>
2020-07-17 11:01:18 +0900

    rename cockbooks -> cookbooks

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-include_recipe './cockbooks/homebrew.rb'
-include_recipe './cockbooks/packages.rb'
-include_recipe './cockbooks/apps.rb'
-include_recipe './cockbooks/dotfiles.rb'
+include_recipe './cookbooks/homebrew.rb'
+include_recipe './cookbooks/packages.rb'
+include_recipe './cookbooks/apps.rb'
+include_recipe './cookbooks/dotfiles.rb'
